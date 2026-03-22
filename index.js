12a87a02d9c0b3ba463e775f9ead9db700ada96f
mcworldbackup0406-sketch <mcworldbackup0406@gmail.com>
2026-03-22 15:13:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ function createBot() {
   bot.loadPlugin(pathfinder);
   const mcData = require('minecraft-data')(bot.version);
   const defaultMove = new Movements(bot, mcData);
-  bot.settings.colorsEnabled = false;
 
   let pendingPromise = Promise.resolve();
 
